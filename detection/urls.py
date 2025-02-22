86872a55adbd9ba2a92136c074e8b617cf63d55d
ann <suprapa.phrosuan@gmail.com>
2025-02-22 23:46:43 +0700

    ....

--- detection/urls.py
+++ detection/urls.py
@@ -1,21 +1,6 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('C01/', views.cobrastretch, name='cobrastretch'),
-    path('C02/', views.childpose, name='childpose'),
-    path('C03/', views.standingforwardbend, name='standingforwardbend'),
-    path('C04/', views.overheadtricepstretch_L, name='overheadtricepstretch_L'),
-    path('C05/', views.overheadtricepstretch_R, name='overheadtricepstretch_R'),
-    path('C06/', views.quadricepstretch_L, name='quadricepstretch_L'),
-    path('C07/', views.quadricepstretch_R, name='quadricepstretch_R'),
-    path('C08/', views.shoulderstretch_R, name='shoulderstretch_R'),
-    path('C09/', views.shoulderstretch_L, name='shoulderstretch_L'),
-    path('E01/', views.elbowplank, name='elbowplank'),
-    path('W04/', views.neckstretch_R, name='neckstretch_R'),
-    path('W05/', views.neckstretch_L, name='neckstretch_L'),
-    path('W06/', views.reachingup, name='reachingup'),
-    path('W07/', views.reachingdown, name='reachingdown'),
-    path('W09/', views.standingsidebend_R, name='standingsidebend_R'),
+    path('<str:code>/', views.exercise_view, name='exercise_view'),
 ]
--- detection/views.py
+++ detection/views.py
@@ -1,49 +1,27 @@
 from django.shortcuts import render
 
-def cobrastretch(request):
-    return render(request, 'detection/cobrastretch.html', {'code': 'C01'})
+def exercise_view(request, code):
+    templates = {
+        'C01': 'detection/cobrastretch.html',
+        'C02': 'detection/childpose.html',
+        'C03': 'detection/standingforwardbend.html',
+        'C04': 'detection/overheadtricepstretch_L.html',
+        'C05': 'detection/overheadtricepstretch_R.html',
+        'C06': 'detection/quadricepstretch_L.html',
+        'C07': 'detection/quadricepstretch_R.html',
+        'C08': 'detection/shoulderstretch_R.html',
+        'C09': 'detection/shoulderstretch_L.html',
+        'E01': 'detection/elbowplank.html',
+        'W04': 'detection/neckstretch_R.html',
+        'W05': 'detection/neckstretch_L.html',
+        'W06': 'detection/reachingup.html',
+        'W07': 'detection/reachingdown.html',
+        'W08': 'detection/standingsidebend_L.html',
+        'W09': 'detection/standingsidebend_R.html', 
+    }
 
-def childpose(request):
-    return render(request, 'detection/childpose.html', {'code': 'C02'})
-
-def standingforwardbend(request):
-    return render(request, 'detection/standingforwardbend.html', {'code': 'C03'})
-
-def overheadtricepstretch_L(request):
-    return render(request, 'detection/overheadtricepstretch_L.html', {'code': 'C04'})
-
-def overheadtricepstretch_R(request):
-    return render(request, 'detection/overheadtricepstretch_R.html', {'code': 'C05'})
-
-def quadricepstretch_L(request):
-    return render(request, 'detection/quadricepstretch_L.html', {'code': 'C06'})
-
-def quadricepstretch_R(request):
-    return render(request, 'detection/quadricepstretch_R.html', {'code': 'C07'})
-
-def shoulderstretch_R(request):
-    return render(request, 'detection/shoulderstretch_R.html', {'code': 'C08'})
-
-def shoulderstretch_L(request):
-    return render(request, 'detection/shoulderstretch_L.html', {'code': 'C09'})
-
-def elbowplank(request):
-    return render(request, 'detection/elbowplank.html', {'code': 'E01'})
-
-def neckstretch_R(request):
-    return render(request, 'detection/neckstretch_R.html', {'code': 'W04'})
-
-def neckstretch_L(request):
-    return render(request, 'detection/neckstretch_L.html', {'code': 'W05'})
-
-def reachingup(request):
-    return render(request, 'detection/reachingup.html', {'code': 'W06'})
-
-def reachingdown(request):
-    return render(request, 'detection/reachingdown.html', {'code': 'W07'})
-
-def standingsidebend_R(request):
-    return render(request, 'detection/standingsidebend_R.html', {'code': 'W09'})
-
-def standingsidebend_L(request):
-    return render(request, 'detection/standingsidebend_R.html', {'code': 'W08'})
+    template = templates.get(code)
+    if template:
+        return render(request, template, {'code': code})
+    else:
+        return render(request, 'detection/error.html', {'message': 'Invalid code'})
